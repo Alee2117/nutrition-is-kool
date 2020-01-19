a0dceff714b6f2c5744e2abd76f73475ed36cc21
Austin Lee <alee2117@gmail.com>
2020-01-19 12:24:15 -0500

    Edited code to run more efficiently. Fixed API call running at startup

--- src/Components/Nutrition/Nutrition.js
+++ src/Components/Nutrition/Nutrition.js
@@ -2,31 +2,37 @@ import React, { useState, useEffect } from "react";
 
 const Nutrition = () => {
   const [food, setFood] = useState("");
+  const [search, setSearch] = useState("");
+  const [isLoaded, setisLoaded] = useState(false);
 
-  const getInput = event => {
-    event.preventDefault();
-    console.log(event.target.value);
-    setFood(event.target.value);
+  const getInput = e => {
+    e.preventDefault();
+    setSearch(e.target.value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
+    setFood(e.target.value);
+    setisLoaded(true);
   };
-
-  useEffect(() => {
+  const getNutrition = food => {
     const appId = "e124e5bc";
     const appKey = "fd3e0319818416b4e8496e3502bcb565";
-    const userInput = food;
-
     fetch(
-      `https://api.nutritionix.com/v1_1/search/${userInput}?&appId=${appId}&appKey=${appKey}`
+      `https://api.nutritionix.com/v1_1/search/${food}?&appId=${appId}&appKey=${appKey}`
     )
       .then(response => {
         return response.json();
       })
       .then(responseData => {
-        console.log(responseData.hits);
+        console.log(responseData.hits[0].fields);
       });
+  };
+
+  useEffect(() => {
+    if (isLoaded) {
+      getNutrition(search);
+    }
   }, [food]);
 
   return (
@@ -39,7 +45,7 @@ const Nutrition = () => {
           name="enteredFood"
           onChange={getInput}
         ></input>
-        <input type="submit" onClick={getInput}></input>
+        <input type="submit"></input>
       </form>
       <h1>Hello!</h1>
       <p>Current state = {food}</p>
